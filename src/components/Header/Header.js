f8b7174b590213a86fc89747c6dea72dac9e3c4e
Maxime Coucke <maxime.coucke@etu.univ-orleans.fr>
2018-12-27 19:41:20 +0100

    editing header, adding sections

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './style.css';
 
 class Header extends React.Component {
     render() {
       return (
       <header>
-        
+          <div />
+          <div className="section">
+              <p>À propos</p>
+          </div>
+          <div className="section">
+              <p>Parcours</p>
+          </div>
+          <div className="section">
+              <p>Réalisations</p>
+          </div>
+          <div className="section">
+              <p>Contact</p>
+          </div>
+          <div />
+
       </header>
       );
     }
