f5867f3bf619c923fc7c78be9730293cf5baefc7
Maxime Coucke <maxime.coucke@etu.univ-orleans.fr>
2018-12-27 20:02:12 +0100

    adding about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
+import AboutPage from "./pages/AboutPage/AboutPage";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header/>
+        <AboutPage/>
       </div>
     );
   }
---
+++ src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+const AboutPage = props => (
+    <div>
+        <h1>Maxime</h1>
+        <h1>COUCKE</h1>
+        <h2>Étudiant en Informatique</h2>
+        <p>Actuellement en Licence 3 Ingénierie informatique à l'Université
+            d'Orléans,</p>
+        <p>je cherche à développer mes connaissances autour
+        des nouvelles technologies. </p>
+        <p>Vous trouverez ici une présentation
+        de mon parcours et quelques-unes de mes réalisations.</p>
+    </div>
+);
+
+export default AboutPage;
