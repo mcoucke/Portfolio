942a3795ea4dedce4419ce9242bb4eb25faa7a0a
Maxime Coucke <maxime.coucke@etu.univ-orleans.fr>
2019-01-04 12:46:24 +0100

    editing aboutpage, adding animation and default font size for app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
-import AboutPage from "./pages/AboutPage/AboutPage";
+import AboutAnimation from "./components/AboutAnimation/AboutAnimation";
 import StudiesPage from "./pages/StudiesPage/StudiesPage";
 import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
           <Header/>
-          <AboutPage/>
+          <AboutAnimation/>
           <StudiesPage/>
           <ProjectsPage/>
           <ContactPage/>
---
+++ src/components/AboutAnimation/AboutAnimation.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components';
+import { StaggeredMotion, spring } from "react-motion";
+import './style.css';
+
+const Container = styled.div`
+    display: flex;
+    padding-top: 5rem;
+    min-height: 100vh;
+    flex: 0 1 auto;
+    flex-grow: 0;
+    flex-shrink: 1;
+    flex-basis: auto;
+    flex-direction: column;
+    `;
+
+class AboutAnimation extends React.Component {
+    render() {
+        return (
+            <StaggeredMotion
+                defaultStyles={[
+                    {currentOpacity: 0},
+                    {currentOpacity: 0},
+                    {currentOpacity: 0},
+                    {currentOpacity: 0},
+                ]}
+                styles={(prevStyles) => [
+                    {currentOpacity: spring(1, { stiffness: 50, damping: 35 })},
+                    {currentOpacity: spring(prevStyles[0].currentOpacity, { stiffness: 140, damping: 25 })},
+                    {currentOpacity: spring(prevStyles[1].currentOpacity, { stiffness: 110, damping: 30 })},
+                    {currentOpacity: spring(prevStyles[2].currentOpacity, { stiffness: 80, damping: 35 })},
+                ]}
+            >
+                {(styles) => (
+                    <Container>
+                        <h1 style={{opacity: styles[0].currentOpacity}}>Maxime COUCKE</h1>
+                        <h2 style={{opacity: styles[1].currentOpacity}}>Étudiant en Informatique</h2>
+                        <p style={{opacity: styles[2].currentOpacity}}>
+                            Actuellement en Licence 3 Ingénierie informatique à l'Université d'Orléans</p>
+                        <p style={{opacity: styles[3].currentOpacity}}>
+                            J'étudie toute sorte de technologies : Développement logiciel, web, mobile </p>
+                    </Container>
+                )}
+            </StaggeredMotion>
+        );
+    }
+}
+
+export default AboutAnimation;
